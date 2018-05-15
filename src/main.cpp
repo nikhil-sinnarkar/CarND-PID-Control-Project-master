01a484bcf508b2f435b82fe2d80871914845d81c
nikhil-sinnarkar <35420772+nikhil-sinnarkar@users.noreply.github.com>
2018-05-15 16:25:23 +0530

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double init_Kp = -0.35;
+  double init_Kp = -0.25;
   double init_Ki = -0.001;
-  double init_Kd = -4.0;
+  double init_Kd = -6.5;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -64,12 +64,19 @@ int main()
           pid.UpdateError(cte);
           steer_value = pid.TotalError();
           
+          double t = 0.0;
+          if((angle >= 0.5 || angle <= -0.5) && speed >= 40){
+            t = 0.25;
+          }
+          else{
+            t = 0.6;
+          }
           // DEBUG
           // std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = t;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           // std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
